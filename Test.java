b46cfe69242f4527690e51b7ca35113d171d17f6
tanu-mk <tanujamk.xworkz@gmail.com>
2022-10-13 19:33:32 +0530

    committing test files

---
+++ Test.java
@@ -0,0 +1,44 @@
+1. Platform Independent 
+			-Which converts .java file into .class file 
+			-Java is platform independent means java can be used in any platform like windows, linux, ubuntu etc
+			-Byte code is Platform Independent 
+			-source code is converted to byte code with the help of java compiler, byte code is platform independent
+
+2. Primitive DataTypes
+			- DataTypes which starts with lower case 
+			- There are 8 types of primitive datatypes(int, long, char, float, double, boolean, short, byte)
+			
+			
+			
+   Non-Primitive DataTypes
+			- DataTypes which starts with upper case
+			- There are n number of non primitive datatypes(String , ClassName, etc)
+			
+3. Class 
+		-class is a design or blueprint of states and behaviour 
+		-Where states are referred to as variables and behaviour are referred to as methods
+		-class members consist of variables, method, constructor, blocks
+		-Variables are the registered area allocated in a memory
+		-method is a collection of statements which are grouped together to perform functionality 
+		-constructor is a special type of method which will be same name as class name with no return type
+
+6. public static void main(String t[])
+
+		public - access modifier 
+		static - access modifier
+		void - return type
+		main - method
+		String t[] - argument line 
+		
+
+
+
+
+			
+			
+			
+
+
+
+
+
